3ee87451c649a9df1686487e2ac87f889a7da4eb
Sebastian Kürten <sebastian@topobyte.de>
2020-10-08 20:46:55 +0200

    Implement loading of CSV data from RKI

---
+++ project/core-testing/src/test/java/de/mobanisto/covidplz/TestLoadDailyRKIData.java
@@ -0,0 +1,48 @@
+// Copyright (c) 2020 Mobanisto UG (haftungsbeschränkt)
+//
+// This file is part of covid-plz.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package de.mobanisto.covidplz;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.mobanisto.covidplz.model.DailyData;
+import de.topobyte.system.utils.SystemPaths;
+
+public class TestLoadDailyRKIData
+{
+
+	@Test
+	public void test() throws IOException
+	{
+		Path repo = SystemPaths.CWD.getParent().getParent();
+		Path file = repo.resolve("data/daily/2020_10_06.csv");
+
+		DailyData data = DailyDataLoader.load(file);
+
+		Assert.assertEquals(412, data.getRsToRegionData().size());
+	}
+
+}
---
+++ project/core/src/main/java/de/mobanisto/covidplz/rki/Fields.java
@@ -0,0 +1,44 @@
+// Copyright (c) 2020 Mobanisto UG (haftungsbeschränkt)
+//
+// This file is part of covid-plz.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package de.mobanisto.covidplz.rki;
+
+public class Fields
+{
+
+	// OBJECTID,ADE,GF,BSG,RS,AGS,SDV_RS,GEN,BEZ,IBZ,BEM,NBD,
+	// SN_L,SN_R,SN_K,SN_V1,SN_V2,SN_G,FK_S3,NUTS,RS_0,AGS_0,
+	// WSK,EWZ,KFL,DEBKG_ID,death_rate,cases,deaths,cases_per_100k,
+	// cases_per_population,BL,BL_ID,county,last_update,cases7_per_100k,
+	// recovered,SHAPE_Length,SHAPE_Area
+
+	public static final String RS = "RS";
+	public static final String DEATH_RATE = "death_rate";
+	public static final String CASES = "cases";
+	public static final String DEATHS = "deaths";
+	public static final String CASES_PER_100K = "cases_per_100k";
+	public static final String CASES_PER_POPULATION = "cases_per_population";
+	public static final String BL = "BL";
+	public static final String LAST_UPDATE = "last_update";
+	public static final String CASES7_PER_100K = "cases7_per_100k";
+
+}
